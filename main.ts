473f651bb984ceb4d877ae9bf4f4e1bb2fac6e56
yus840332 <70998708+yus840332@users.noreply.github.com>
2020-09-09 14:49:02 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+music.onEvent(MusicEvent.MelodyNotePlayed, function () {
+	
+})
+basic.forever(function () {
+    basic.showString("GO")
+    music.playMelody("- - - - - - - - ", 120)
+    music.playMelody("- - - - - - - - ", 120)
+    music.playMelody("- - - - - - - - ", 120)
+    music.playMelody("- - - - - - - - ", 120)
+    music.playMelody("- - - - - - - - ", 120)
+    music.playMelody("- - - - - - - - ", 120)
+    music.playMelody("- - - - - - - - ", 120)
+    music.playMelody("- - - - - - - - ", 120)
+    music.playMelody("- - - - - - - - ", 120)
+})
